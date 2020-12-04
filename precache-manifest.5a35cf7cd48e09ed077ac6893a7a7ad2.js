1ff3f0cf1e5ec88a1ee3a88f5dfa4fe38fc7fa84
Mitch4sho <mitchelloliver1991@gmail.com>
2020-12-03 19:56:39 -0800

    Updates

---
+++ precache-manifest.5a35cf7cd48e09ed077ac6893a7a7ad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ca507a48658472ec6004aa2e408792",
+    "url": "/MovieDB/index.html"
+  },
+  {
+    "revision": "77697cdb3f7710bd80ba",
+    "url": "/MovieDB/static/css/main.911263fc.chunk.css"
+  },
+  {
+    "revision": "7c2c5b95bf07763d14de",
+    "url": "/MovieDB/static/js/2.890500c3.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/MovieDB/static/js/2.890500c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77697cdb3f7710bd80ba",
+    "url": "/MovieDB/static/js/main.25194e7e.chunk.js"
+  },
+  {
+    "revision": "31153f999568876305e1",
+    "url": "/MovieDB/static/js/runtime-main.012e7ca2.js"
+  }
+]);
